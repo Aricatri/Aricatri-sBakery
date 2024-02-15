c77983fa406487f04712932f9620a92ff3fafac7
Manzanita <148496944+Aricatri@users.noreply.github.com>
2024-02-15 11:10:46 -0500

    Add files via upload

---
+++ uno.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.container_responsive'); // Selecciona el elemento con la clase 'containerr'
+    sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.container_responsive'); // Selecciona el elemento con la clase
+    sidebar.style.display = 'none';
+}
